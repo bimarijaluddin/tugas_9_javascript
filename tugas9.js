ad995cd96258c9de6d738142172f882ab3c1b527
bima rijaluddin <bimarijal@gmail.com>
2019-09-19 11:41:46 +0800

    Tugas 9 javascript
    tugas dari niomic

---
+++ tugas9.js
@@ -0,0 +1,13 @@
+function tugas9(){
+    var siswa = {
+        nama : "candralistya",
+        umur : 17,
+        kelas : "XI",
+        jurusan : "Asisten Keperawatan",
+    }
+    for(var x in siswa){
+        console.log(siswa[x])
+    }
+}
+
+tugas9()
